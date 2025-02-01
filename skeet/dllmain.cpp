6b21c1e8043bca6594be76882895494c1ce32068
Ivan Zhyhailo <ghettoboy@gmail.com>
2025-02-01 19:03:27 +0200

    first commit

---
+++ skeet/dllmain.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "skCrypter.h"
+
+static void thread(HMODULE base) {
+    skeet_t* skeet = skeet_t::getInstance(base);
+    if (!skeet->map())
+      std::cout << skCrypt("[t.me/g00d_crack] failed to allocate memory, reboot PC\n");
+    while (GetModuleHandleA("serverbrowser.dll") == 0);
+    system("cls");
+    std::cout << skCrypt("[t.me/g00d_crack] cracked by qhide.\n$$$ Buy SUGAR ELITE for EFT $$$\nSubscribe to t.me/raze_club");
+    skeet->fix_imports();
+    std::cout << skCrypt("[t.me/g00d_crack] fixed!\n");
+    skeet->extra();
+    skeet->entry();
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        AllocConsole();
+        SetConsoleTitleA("t.me/g00d_crack / t.me/raze_club");
+        freopen("CONOUT$", "w", stdout);
+        CreateThread(0, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(thread), hModule, 0, 0);
+        break;
+    }
+    return TRUE;
+}
+
